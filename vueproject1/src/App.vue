922cd8edf622692976714b1115066f3b2d89760e
Simon Sy <simon22sy@gmail.com>
2022-09-01 15:41:07 -0500

    Scaffolding out question card components

--- vueproject1/src/App.vue
+++ vueproject1/src/App.vue
@@ -1,23 +1,33 @@
 <template>
-    <div>
-        Hello World
+    <div class="questions-container">
+        <QuestionCard v-for="item in questions"
+                      :question="item"
+                      :key="item.id"></QuestionCard>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import QuestionCard from './components/QuestionCard.vue'
+import { StackOverflowService } from './services/StackOverflowService'
+import { Question } from './models/Question'
+
+let questions: Question[] = [];
 
 export default defineComponent({
   name: 'App',
   components: {
-    },
-    data() {
-        return {
-            questions: []
-        }
-    },
-    async created() {
+    QuestionCard
+  },
+  data() {
+    return {
+        questions
     }
+  },
+  async created() {
+    const stackOverflowService = new StackOverflowService();
+    this.questions = await stackOverflowService.getQuestions();
+  }
 });
 </script>
 
@@ -29,5 +39,10 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.questions-container{
+    display: flex;
+    flex-direction: column;
 }
 </style>
---
+++ vueproject1/src/components/QuestionCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+        {{question.title}}
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Question } from '@/models/Question'
+
+export default defineComponent({
+  name: 'QuestionCard',
+  components: {
+    },
+    props: {
+        question: Question
+    }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.card {
+    border: 1px solid;
+    border-radius: 8px;
+    padding: 5px;
+    margin: 5px 0px;
+}
+</style>
